a923eb6dceed18ac80d33af6dd65d726eb6fa295
doomiZh <cabuum20mdfk@gmail.com>
2026-03-23 09:50:15 +0100

    Se agrega el Archivo accesos en el proyecto

---
+++ src/modelos/Accesos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ * Clase donde almacena los Accesos de la base de datos
+ * @author grupo
+ */
+public class Accesos {
+    
+}
