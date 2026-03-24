b917651853a60f03c74c381c2d262be3b73886a1
doomiZh <cabuum20mdfk@gmail.com>
2026-03-24 10:21:18 +0100

    Se agrega el metodo que obtiene el listado de articulos totales en Conexion

--- src/bbdd/Conexion.java
+++ src/bbdd/Conexion.java
@@ -131,4 +131,36 @@ public class Conexion {
         }
     }
     
+    /* OPCIONES USUARIO */
+    
+    /**
+     * Método para imprimr el listado de articulo en un JTable donde tendra
+     * el codigo, nombre, categoria y precio de venta del producto o artículo.
+     * @param modelo JTable donde se cargará los datos.
+     */
+    public static void ObtenerListadoArticulo(DefaultTableModel modelo){
+        Object datos[] = new Object[4];
+        String consulta = "select codProducto, nombre, categoria, precio_venta from producto";
+        Conectar();
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(consulta);
+            while (rs.next()) {
+                datos[0] = rs.getString(1);
+                datos[1] = rs.getString(2);
+                datos[2] = rs.getString(3);
+                datos[3] = rs.getDouble(4);
+                modelo.addRow(datos);
+            }
+        } catch (SQLException ex) {
+            System.getLogger(Conexion.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        } finally {
+            Cerrar();
+        }
+    }
+    
+    
+    
+    
+    
 }// End Class
