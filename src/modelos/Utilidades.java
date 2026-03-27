b955a05d33060dfe7a08f82024bb88ea9842432e
doomiZh <cabuum20mdfk@gmail.com>
2026-03-27 12:32:54 +0100

    Se crea los metodos de utilidades para comprobar campos de texto, combos, metodos para si los campos de texto estan enteros o decimal y sus alertas en la clase Utilidades

--- src/modelos/Utilidades.java
+++ src/modelos/Utilidades.java
@@ -4,10 +4,11 @@
  */
 package modelos;
 
+import java.awt.Color;
 import java.awt.Component;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
@@ -15,23 +16,95 @@ import javax.swing.JTextField;
  * @author Marco Antonio
  */
 public class Utilidades {
-    public static boolean ComprobarTextoVacio(JPanel panelDatos) {
-        for (Component c : panelDatos.getComponents()) {
-            if (c instanceof JTextField datos) {
-                if (datos.getText().isBlank()) {
-                    JOptionPane.showMessageDialog(null, "El dato " + datos.getName() + "es obligatorio");
+
+    /**
+     * Método util para comprobar los campos de texto y combos si estan vacios o seleccionados
+     * enviando un mensaje de error caso false, en caso true continuara el codigo. Para obtener los campos de manera escalonada
+     * se pedira de parametros un varargs (Argumentos Variables) para hacer que al momento que pediremos los campos a validar
+     * se pueda colocar los campos de manera ordenada, asi el panel no valida de manera deshordenada
+     * @param panelDatos varargs (Argumento Variable) de tipo Component para obtener los componentes que se 
+     * desee validar.
+     * @return true si los campos estan correctos y false en caso uno de ellos no se valide su funcion,
+     * es decir, que el campo esta vacio o que el campo no esta seleccionado
+     */
+    public static boolean ComprobarCampos(Component... panelDatos) {
+        for (Component c : panelDatos) {
+            if (c instanceof JTextField texto) {
+                if (texto.getText().isBlank()) {
+                    JOptionPane.showMessageDialog(null, "El campo " + texto.getName() + " es obligatorio");
+                    texto.setBackground(Color.red);
+                    return false;
+                }
+            }
+            if (c instanceof JTextArea texto){
+                if (texto.getText().isBlank()) {
+                    JOptionPane.showMessageDialog(null, "El campo " + texto.getName() + " es obligatorio");
+                    texto.setBackground(Color.red);
+                    return false;
+                }
+            }
+            if (c instanceof JComboBox campo) {
+                if (campo.getSelectedIndex() == 0) {
+                    JOptionPane.showMessageDialog(null, "El campo " + campo.getName() + " debe estar seleccionado");
                     return false;
                 }
             }
         }
         return true;
     }
+
+    /**
+     * Método util para comprobar si el campo es un número entero.
+     * Esta funcion intentará cambiar el valor tecleado si es un entero o no.
+     * @param campo JTextField del campo donde validará si es un entero o no.
+     * @return true si es correcto y false en caso contrario.
+     */
+    public static boolean ComprobarNumeroEntero (JTextField campo){
+        String numeroTecleado = campo.getText();
+        int minumero;    
+        try {
+            minumero = Integer.parseInt(numeroTecleado.trim());
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+    }
     
-    public static boolean ComboSeleccionado(JComboBox campo) {
-        if (campo.getSelectedIndex() == 0) {
-            JOptionPane.showMessageDialog(null, "El dato " + campo.getName() + "es obligatorio");
-                    return false;
-        } return true;
+    /**
+     * Método util para comprobar si el campo es un número decimal.
+     * Esta funcion intentará cambiar el valor tecleado si es un decimal o no.
+     * @param campo JTextField del campo donde validará si es un decimal o no.
+     * @return true si es correcto y false en caso contrario.
+     */
+    public static boolean ComprobarNumeroDecimal (JTextField campo){
+        String numeroTecleado = campo.getText();
+        double minumero;    
+        try {
+            minumero = Double.parseDouble(numeroTecleado.trim());
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+    }
+    
+    /**
+     * Método util para mandar una alerta sobre que el numero no es un entero, 
+     * y el campo lo colorea a rojo
+     * @param campo JTextField donde el numero no es un entero
+     */
+    public static void AlertaNumeroEntero(JTextField campo){
+        JOptionPane.showMessageDialog(null, "El campo "+campo.getName()+" solo acepta números enteros.", "Número", JOptionPane.INFORMATION_MESSAGE);
+        campo.setBackground(Color.red);
+    }
+    
+    /**
+     * Método util para mandar una alerta sobre que el numero no es un entero, 
+     * y el campo lo colorea a rojo
+     * @param campo JTextField donde el numero no es un decimal
+     */
+    public static void AlertaNumeroDecimal(JTextField campo){
+        JOptionPane.showMessageDialog(null, "El campo "+campo.getName()+" solo acepta números decimales.", "Número", JOptionPane.INFORMATION_MESSAGE);
+        campo.setBackground(Color.red);
     }
     
 } //Fin de la clase
